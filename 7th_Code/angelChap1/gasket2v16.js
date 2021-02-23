4aa9ea4f268e6b0119dc86e5eec76090701dcd1d
allem40306 <allem40306@gmail.com>
2021-02-23 14:36:18 +0800

    upload 5th ppt and textbook, 7th ppt and code

---
+++ 7th_Code/angelChap1/gasket2v16.js
@@ -0,0 +1,138 @@
+
+var canvas;
+var gl;
+
+var time = 0;
+var dt = 1.0/60.0;
+var timeLoc;
+
+
+var points = [];
+var colors = [];
+
+var NumTimesToSubdivide = 5;
+
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+        
+    //
+    //  Initialize our data for the Sierpinski Gasket
+    //
+
+    // First, initialize the corners of our gasket with three points.
+    
+    var vertices = [
+        vec2( -1, -1 ),
+        vec2(  0,  1 ),
+        vec2(  1, -1 )
+    ];
+
+    divideTriangle( vertices[0], vertices[1], vertices[2],
+                    NumTimesToSubdivide);
+
+    //
+    //  Configure WebGL
+    //
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    //  Load shaders and initialize attribute buffers
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    // Load the data into the GPU
+    
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+    
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+
+    timeLoc = gl.getUniformLocation(program, "time");
+
+    render();
+};
+
+function triangle( a, b, c, color )
+{
+
+    // add colors and vertices for one triangle
+
+    var baseColors = [
+        vec3(1.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        vec3(0.0, 0.0, 1.0),
+        vec3(0.0, 0.0, 0.0)
+    ];
+
+    colors.push( baseColors[color] );
+    points.push( a );
+    colors.push( baseColors[(color+1)%4] );
+    points.push( b );
+    colors.push( baseColors[(color+2)%4] );
+    points.push( c );
+}
+
+function divideTriangle( a, b, c, count, color )
+{
+
+    // check for end of recursion
+    
+    if ( count === 0 ) {
+        triangle( a, b, c, color );
+    }
+    else {
+    
+        //bisect the sides
+        
+        var ab = mix( a, b, 0.5 );
+        var ac = mix( a, c, 0.5 );
+        var bc = mix( b, c, 0.5 );
+
+        --count;
+
+        // three new triangles
+        
+          divideTriangle( a, ab, ac, count, 0 );
+          divideTriangle( c, ac, bc, count, 1 );
+          divideTriangle( b, bc, ab, count,2 );   
+          divideTriangle( bc, ac, ab, count,3 );
+
+    }
+}
+
+function render()
+{
+
+    time+=dt;
+    gl.uniform1f(timeLoc, time);
+    gl.clear( gl.COLOR_BUFFER_BIT);
+  //  gl.drawArrays( gl.LINE_LOOP, 0, points.length );
+   gl.drawArrays( gl.TRIANGLES, 0, points.length );
+
+    requestAnimFrame(render);
+
+
+
+ //   gl.clear( gl.COLOR_BUFFER_BIT );
+ //   gl.drawArrays( gl.TRIANGLES, 0, points.length );
+}
+
